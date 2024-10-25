1fc7dccaa1d0cd89d2198c4bec0e67d00123539f
shahnawroz <sh.nawrose@gmail.com>
2024-10-25 17:03:13 +0600

    change design

--- src/app/component/HeroComponent.tsx
+++ src/app/component/HeroComponent.tsx
@@ -3,10 +3,10 @@ import React from 'react';
 
 const HeroComponent = () => {
   return (
-    <section className="hero-section bg-white flex items-center justify-center h-screen">
+    <section className="hero-section bg-black flex items-center justify-center h-screen">
       <div className="text-center px-4">
-        <h1 className="text-4xl font-bold mb-4 text-black">Welcome to MIANS Website</h1>
-        <p className="text-lg mb-8 text-black">We provide top-notch IT solutions.</p>
+        <h1 className="text-4xl font-bold mb-4 text-white">Welcome to MIANS Website</h1>
+        <p className="text-lg mb-8 text-white">We provide top-notch IT solutions.</p>
         
       </div>
     </section>
--- src/app/component/common/HeaderComponent.tsx
+++ src/app/component/common/HeaderComponent.tsx
@@ -1,19 +1,22 @@
 // components/Header.tsx
 import React, { useState } from 'react';
 import Link from 'next/link';
+import Image from 'next/image';
 
 const HeaderComponent = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
 
   return (
-    <header className="fixed w-full bg-black shadow-md z-10 p-4">
+    <header className="fixed w-full bg-white shadow-md z-10 p-4">
       <div className="flex justify-between items-center">
-        <div className="logo text-white">My Logo</div>
+      <Link href="/" className="logo text-black">
+          <Image src="/images/logo.png" alt="MIANS" width={100} height={20} />
+        </Link>
         
         {/* Hamburger Icon */}
         <button
           onClick={() => setIsMenuOpen(!isMenuOpen)}
-          className="text-white md:hidden"
+          className="text-black md:hidden"
         >
           <svg
             className="w-6 h-6"
@@ -33,13 +36,13 @@ const HeaderComponent = () => {
 
         {/* Desktop Navigation */}
         <nav className="hidden md:flex space-x-4">
-          <Link href="#services" className="hover:text-blue-500 text-white">
+          <Link href="#services" className="border-b-2 border-transparent hover:border-b-gray-600 transition-all duration-300 text-black">
             Our Team
           </Link>
-          <Link href="#testimonials" className="hover:text-blue-500 text-white">
+          <Link href="#testimonials" className="border-b-2 border-transparent hover:border-b-gray-600 transition-all duration-300 text-black">
             Case Study
           </Link>
-          <Link href="#contact" className="hover:text-blue-500 text-white">
+          <Link href="#contact" className="border-b-2 border-transparent hover:border-b-gray-600 transition-all duration-300 text-black">
             Contact
           </Link>
         </nav>
@@ -47,24 +50,24 @@ const HeaderComponent = () => {
 
       {/* Mobile Navigation with Animation */}
       {isMenuOpen && (
-        <nav className="md:hidden flex flex-col mt-4 space-y-2 bg-black items-end">
+        <nav className="md:hidden flex flex-col mt-4 space-y-2 bg-white items-end">
           <div className="flex flex-col items-end">
-            <Link href="#services" className="text-white">
+            <Link href="#services" className="text-black">
               Our Team
             </Link>
-            <div className="w-full h-[2px] bg-white mt-1"></div>
+            <div className="w-full h-[2px] bg-black mt-1"></div>
           </div>
           <div className="flex flex-col items-end">
-            <Link href="#testimonials" className="text-white">
+            <Link href="#testimonials" className="text-black ">
               Case Study
             </Link>
-            <div className="w-full h-[2px] bg-white mt-1"></div>
+            <div className="w-full h-[2px] bg-black mt-1"></div>
           </div>
           <div className="flex flex-col items-end">
-            <Link href="#contact" className="text-white">
+            <Link href="#contact" className="text-black">
               Contact
             </Link>
-            <div className="w-full h-[2px] bg-white mt-1"></div>
+            <div className="w-full h-[2px] bg-black mt-1"></div>
           </div>
         </nav>
        )}
